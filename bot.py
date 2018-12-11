b2dac0ac63d5f621cae1542163b399f6982fb6a9
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-11 18:09:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,12 @@ import json
 from os import environ
  
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = commands.Bot(description="RadHok Official Bot", command_prefix=commands.when_mentioned_or("^"), pm_help = True)
+client = commands.Bot(description="RadHok Official Bot", command_prefix=commands.when_mentioned_or(">"), pm_help = True)
 client.remove_command('help')
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name='for ^help'))
+        await client.change_presence(game=discord.Game(name='for >help'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)

d5d9b237a927f59b95c704132f0bebabed76d984
Noob Gamer <39646826+biswajit9123@users.noreply.github.com>
2018-12-23 15:20:26 +0530

    Update radhok.py

--- radhok.py
+++ radhok.py
@@ -62,6 +62,45 @@ def get_xp(user_id: int):
         return users[user_id]['xp']
     else:
         return 0
+     with open("users.json", "r") as f:
+        users = json.load(f)
+
+        if message.author.bot:
+            return
+        if message.channel.is_private:
+            return
+        else:
+            await update_data(users, message.author, message.server)
+            number = random.randint(5,10)
+            await add_experience(users, message.author, number, message.server)
+            await level_up(users, message.author, message.channel, message.server)
+
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+    await bot.process_commands(message)
+
+async def update_data(users, user, server):
+    if not user.id + "-" + server.id in users:
+        users[user.id + "-" + server.id] = {}
+        users[user.id + "-" + server.id]["experience"] = 0
+        users[user.id + "-" + server.id]["level"] = 1
+        users[user.id + "-" + server.id]["last_message"] = 0
+
+async def add_experience(users, user, exp, server):
+    if time.time() - users[user.id + "-" + server.id]["last_message"] > 5: 
+        users[user.id + "-" + server.id]["experience"] += exp
+        users[user.id + "-" + server.id]["last_message"] = time.time()
+    else:
+        return
+
+async def level_up(users, user, channel, server):
+    experience = users[user.id + "-" + server.id]["experience"]
+    lvl_start = users[user.id + "-" + server.id]["level"]
+    lvl_end = int(experience ** (1/4))
+
+    if lvl_start < lvl_end:
+        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
+        users[user.id + "-" + server.id]["level"] = lvl_end
 
 @client.event
 async def on_reaction_add(reaction, user):

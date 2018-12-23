e5e9d6e45e4d749512245a1b198ce4214274f23f
Noob Gamer <39646826+biswajit9123@users.noreply.github.com>
2018-12-23 15:06:38 +0530

    Update radhok.py

--- radhok.py
+++ radhok.py
@@ -48,54 +48,7 @@ async def level_up(users, user, channel, server):
     if lvl_start < lvl_end:
         await client.send_message(channel, f":tada: Congrats {user.name}, you levelled up to level {lvl_end}!")
         users[user.id + "-" + server.id]["level"] = lvl_end
-    else:
-        return
 
-async def level_on_up(users, user, channel,server):
-    user_add_xp(message.author.id, 2)
-    if message.content.lower().startswith('mv!dailyrank'):
-        r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-        level=int(get_xp(message.author.id)/100)
-        msgs=int(get_xp(message.author.id)/2)
-        embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-        embed.set_author(name='Daily Universal Rank')
-        embed.set_thumbnail(url = message.author.avatar_url)
-        embed.add_field(name = '**__XP__**'.format(message.author),value ='``{}``'.format(get_xp(message.author.id)),inline = False)
-        embed.add_field(name = '**__Level__**'.format(message.author),value ='``{}``'.format(level),inline = False)
-        embed.add_field(name = '**__Messages__**'.format(message.author),value ='``{}`` Messages'.format(msgs),inline = False)
-        embed.add_field(name='Note:',value='Our bot reset all ranks everyday so it shows only daily rank')
-        await client.send_message(message.channel, embed=embed)
-    
-def user_add_xp(user_id: int, xp: int):
-    if os.path.isfile("users.json"):
-        try:
-            with open('users.json', 'r') as fp:
-                users = json.load(fp)
-            users[user_id]['xp'] += xp
-            with open('users.json', 'w') as fp:
-                json.dump(users, fp, sort_keys=True, indent=4)
-        except KeyError:
-            with open('users.json', 'r') as fp:
-                users = json.load(fp)
-            users[user_id] = {}
-            users[user_id]['xp'] = xp
-            with open('users.json', 'w') as fp:
-                json.dump(users, fp, sort_keys=True, indent=4)
-    else:
-        users = {user_id: {}}
-        users[user_id]['xp'] = xp
-        with open('users.json', 'w') as fp:
-            json.dump(users, fp, sort_keys=True, indent=4)
-
-
-def get_xp(user_id: int):
-    if os.path.isfile('users.json'):
-        with open('users.json', 'r') as fp:
-            users = json.load(fp)
-        return users[user_id]['xp']
-    else:
-        return 0
-	               
 #show when it connects to discord
 @client.event
 async def on_ready():

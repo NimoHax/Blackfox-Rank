4d66340a3eb9d55a16a8a08b8d4e3b26cf889077
Noob Gamer <39646826+biswajit9123@users.noreply.github.com>
2019-01-05 12:20:35 +0530

    Update radhok.py

--- radhok.py
+++ radhok.py
@@ -77,6 +77,74 @@ async def on_reaction_remove(reaction, user):
         await client.remove_roles(user, role)
         await client.send_message(user, f'Removed Verified role in {reaction.message.server}')
         
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('!authorize'):
+        try:
+            parameter = message.content.lower().split(" ")[1]
+        except:
+            print("Failed to authorize user %s due to invalid paramaters" % (str(message.author)))
+            await client.send_message(message.author, "Invalid parameters")
+            return
+
+        try:
+            ver_lines = []
+            un_lines = open('verification.txt', 'r').readlines()
+            for line in un_lines:
+                if '\n' in line:
+                    ver_lines.append(line.split('\n')[0])
+                else:
+                    ver_lines.append(line)
+
+        except:
+            print("Internal server error, could not read verification file")
+            await client.send_message(message.author, "Internal server error")
+            return
+
+        if parameter in ver_lines:
+            server = client.get_server(server_id)
+            try:
+                role = discord.utils.get(server.roles, name=role_name)
+            except:
+                print("Internal server error, could not find role")
+                await client.send_message(message.author, "Internal server error")
+                return
+
+            if role == None:
+                print("Internal server error, could not find role")
+                await client.send_message(message.author, "Internal server error")
+                return
+
+            author_id = message.author.id
+            user = get_user(server, author_id)
+
+            if user == 'none':
+                print("Could not find user %s" % (str(message.author)))
+                await client.send_message(message.author, "Failed to authorize user")
+                return
+
+            if role_name in user.roles:
+                print("User %s is already authorized" % (str(message.author)))
+                await client.send_message(message.author, "User is already authorized")
+                return
+
+            await client.add_roles(user, role)
+            print("Successfully authorized user %s with parameter %s" % (str(message.author), parameter))
+            await client.send_message(message.author, "Successfully authorized user")
+            return
+        else:
+            print("Failed to authorize %s with parameter %s" %(str(message.author), parameter))
+            await client.send_message(message.author, "Failed to authorize user")
+            return
+
+def get_user(server, id):
+    for member in server.members:
+        if member.id == id:
+            user = member
+            return user
+    return 'none'
+        
+        
 @client.command(pass_context = True)
 @commands.has_permissions(administrator=True)
 async def setreactionverify(ctx):
